6594a7945b240a16e2a12e12606097269c686c0a
abdouhdd@gmail.com <abdrhmnhdd@gmail.com>
2022-06-10 01:50:49 +0100

    add check controller

---
+++ shop.commerce.api/Controllers/CheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shop.commerce.api.presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Check()
+        {
+            return Ok(new { Status = "Ok" });
+        }
+    }
+}
